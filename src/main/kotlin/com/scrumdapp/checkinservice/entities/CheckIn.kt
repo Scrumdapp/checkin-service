e649dda49e0ad7f386dc6d6af6e621920cd36e74
StevenSeagull1 <stevenhoekstra2005@gmail.com>
2026-03-17 12:37:27 +0100

    Create CheckIn.kt

---
+++ src/main/kotlin/com/scrumdapp/checkinservice/entities/CheckIn.kt
@@ -0,0 +1,45 @@
+package com.scrumdapp.checkinservice.entities
+
+import jakarta.persistence.*
+import java.io.Serializable
+import java.util.*
+
+
+class CheckInId(private val UserId: Int, private val GroupId: Int, private val Date: Date?) : Serializable
+
+@Entity
+@IdClass(CheckInId::class)
+class CheckIn {
+
+    @Id
+    var userId: Int = 0
+
+    @Id
+    @OneToMany(mappedBy = "CheckIn", cascade = [(CascadeType.ALL)])
+    @JoinColumn(referencedColumnName = "id")
+    var groupId: Int = 0
+
+    @Id
+    var date: Date? = null
+
+    var obstacleComment: String? = null
+
+    var presence: Int? = null
+
+    var presenceComment: String? = null
+
+    var checkinStars: Int? = null
+
+    var checkinComment: String? = null
+
+    var checkupStars: Int? = null
+
+    var checkupComment: String? = null
+
+    var checkoutStars: Int? = null
+
+    var checkoutComment: String? = null
+
+
+}
+
